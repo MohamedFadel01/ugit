012a60f253028e533fab813287e28845d0a13ce4
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-06 15:06:08 +0300

    feat: add hash_object command for storing file data as objects

--- ugit/data.py
+++ ugit/data.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 
 GIT_DIR = ".ugit"
@@ -6,4 +7,14 @@ GIT_DIR = ".ugit"
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f"{GIT_DIR}/objects")
+    
+    
+    
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
+        out.write(data)
+        
+    return oid
     
